ec1af96316ad5363f70b151612f835c25ca0f3ec
Rahil-Huseynov <156452922+Rahil-Huseynov@users.noreply.github.com>
2024-01-29 19:12:44 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+document.getElementById(`header`).innerHTML = `    <header>
+<div>
+    <a href="index.html">
+        <p class="logo">oralox</p>
+    </a>
+</div>
+<div>
+    <ul class="nav_item">
+        <li class="nav_items"> <a href="work.html">Work</a></li>
+        <li class="nav_items"> <a href="about.html">About</a></li>
+        <li class="nav_items"> <a href="contact.html">Contact</a</li>
+    </ul>
+</div>
+</header>` ;
+
+
+document.getElementById(`footer`).innerHTML = `<footer>
+<div class="footer_item">
+    <div>
+        <h2 style="color: white;">Oraxol</h2>
+        <p class="footer_item_p">A small river named Duden flows by their place and supplies it with the
+            necessary regelialia.</p>
+    </div>
+    <div>
+        <h2 style="color: white;">EXPLORE</h2>
+        <p class="footer_item_p"><u>About Us</u></p>
+        <p class="footer_item_p"><u>Blog</u></p>
+        <p class="footer_item_p"><u>Work</u></p>
+    </div>
+
+    <div>
+        <h2 style="color: white;">QUICK LINKS</h2>
+        <p class="footer_item_p"><u>Contact Us</u></p>
+        <p class="footer_item_p"><u>Terms & Conditions</u></p>
+        <p class="footer_item_p"><u>Privacy</u></p>
+        <p class="footer_item_p"><u>Feedbacks</u></p>
+    </div>
+
+    <div>
+        <h2 style="color: white;">CATEGORIES</h2>
+        <p class="footer_item_p">Graphic Design</p>
+        <p class="footer_item_p">Web Design</p>
+        <p class="footer_item_p">Branding & Printing</p>
+        <p class="footer_item_p">Web Development</p>
+    </div>
+
+    <div>
+        <h2 style="color: white;">HAVE A QUESTIONS?</h2>
+        <p class="footer_item_p">203 Fake St. Mountain View, San Francisco, California, USA</p>
+        <p class="footer_item_p">+2 392 3929 210</p>
+        <p class="footer_item_p">info@yourdomain.com</p>
+    </div>
+</div>
+
+<div style="border: 1px solid white; height: 1px; width: 100%;"></div>
+
+<div class="footer_finish">
+    <p class="footer_finish_p">Copyright ©2024 All rights reserved | This template is made with ❤ by Colorlib
+    </p>
+</div>
+
+</footer>`
